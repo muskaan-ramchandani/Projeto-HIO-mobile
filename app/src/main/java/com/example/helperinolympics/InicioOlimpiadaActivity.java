d5ad90ce11bd09c6c39eb9d9e71d099987071c8d
Isabela Cunha Sevalho <2022326890@ifam.edu.br>
2024-04-15 11:39:34 -0300

    Update

---
+++ app/src/main/java/com/example/helperinolympics/InicioOlimpiadaActivity.java
@@ -0,0 +1,10 @@
+package com.example.helperinolympics;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class InicioOlimpiadaActivity extends Activity {
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_olimpiada);
+    }
+}
