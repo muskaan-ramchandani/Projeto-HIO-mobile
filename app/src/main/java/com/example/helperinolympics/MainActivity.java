e08a9fc750e0f26e5255d8c5cd1b19cb9b937409
Isabela Cunha Sevalho <2022326890@ifam.edu.br>
2024-06-20 17:33:39 -0300

    Correction of syntax errors

---
+++ app/src/main/java/com/example/helperinolympics/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.helperinolympics;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+public class MainActivity extends Activity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, TelaInicialAlunoActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3500); // 3,5 segundos de splash
+    }
+}
