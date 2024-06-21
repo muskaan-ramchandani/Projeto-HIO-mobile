7e175a03d886f82c6e4857f563092eac7bb9489a
Isabela Cunha Sevalho <2022326890@ifam.edu.br>
2024-06-21 08:48:56 -0300

    Correction of minor errors and starting the student home screen menu

--- app/src/main/java/com/example/helperinolympics/TelaInicialAlunoActivity.java
+++ app/src/main/java/com/example/helperinolympics/TelaInicialAlunoActivity.java
@@ -2,10 +2,15 @@ package com.example.helperinolympics;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
 
 public class TelaInicialAlunoActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicial_aluno);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu){
+
     }
 }
