b6dd56b95309619eba260c27b8e358c00701aede
Isabela Cunha Sevalho <2022326890@ifam.edu.br>
2024-06-25 09:50:06 -0300

    Attempts to fix errors that appear when running

--- app/src/main/java/com/example/helperinolympics/PerfilAlunoActivity.java
+++ app/src/main/java/com/example/helperinolympics/PerfilAlunoActivity.java
@@ -1,8 +1,11 @@
 package com.example.helperinolympics;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 public class PerfilAlunoActivity extends Activity {
-
-
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_perfil_aluno);
+    }
 }
