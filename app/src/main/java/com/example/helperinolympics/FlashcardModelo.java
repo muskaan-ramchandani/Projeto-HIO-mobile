a1e89bbc40ae58115ce28d2c98e958dd54645c62
Isabela Cunha Sevalho <2022326890@ifam.edu.br>
2024-07-10 16:51:00 -0300

    Flashcard screen implementation

---
+++ app/src/main/java/com/example/helperinolympics/FlashcardModelo.java
@@ -0,0 +1,48 @@
+package com.example.helperinolympics;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+
+
+public class FlashcardModelo extends DialogFragment {
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.modelo_flashcard, container, false);
+
+
+        // Configurar o botão de fechar
+        view.findViewById(R.id.btnFechar).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dismiss();
+            }
+        });
+
+        return view;
+    }
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        if (getDialog() != null) {
+            getDialog().getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+            getDialog().getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+        }
+    }
+}
+
+
+
+
+
--- app/src/main/java/com/example/helperinolympics/PerfilAlunoActivity.java
+++ app/src/main/java/com/example/helperinolympics/PerfilAlunoActivity.java
@@ -2,10 +2,30 @@ package com.example.helperinolympics;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.helperinolympics.databinding.ActivityMenuDeslizanteAlunoBinding;
+import com.example.helperinolympics.databinding.ActivityPerfilAlunoBinding;
 
 public class PerfilAlunoActivity extends Activity {
+
+    private ActivityPerfilAlunoBinding binding;
+    Button btnVoltarInicio;
+
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil_aluno);
+
+        binding = ActivityPerfilAlunoBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        btnVoltarInicio= binding.btnIniciar;
+        btnVoltarInicio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
