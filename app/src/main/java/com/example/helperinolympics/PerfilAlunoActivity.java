7d518c5359458f6e5d57194de543671a30393c54
Isabela Cunha Sevalho <2022326890@ifam.edu.br>
2024-07-11 18:35:15 -0300

    Update of the profile screen, implementing a graph relating to the number of hits and errors

--- app/src/main/java/com/example/helperinolympics/PerfilAlunoActivity.java
+++ app/src/main/java/com/example/helperinolympics/PerfilAlunoActivity.java
@@ -2,18 +2,24 @@ package com.example.helperinolympics;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.helperinolympics.databinding.ActivityMenuDeslizanteAlunoBinding;
 import com.example.helperinolympics.databinding.ActivityPerfilAlunoBinding;
+
+import org.eazegraph.lib.charts.PieChart;
+import org.eazegraph.lib.models.PieModel;
 
 public class PerfilAlunoActivity extends Activity {
 
     private ActivityPerfilAlunoBinding binding;
     Button btnVoltarInicio;
-
+    TextView txtAcertos, txtErros;
+    PieChart graficoPizzaEstatisticas;
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil_aluno);
@@ -29,5 +35,28 @@ public class PerfilAlunoActivity extends Activity {
                 startActivity(intent);
             }
         });
+
+        txtAcertos= findViewById(R.id.txtLegendaAcertos);
+        txtErros= findViewById(R.id.txtLegendaErros);
+        graficoPizzaEstatisticas= findViewById(R.id.graficoPizzaErrosAcertos);
+        setData();
+    }
+
+    private void setData(){
+        int numeroAcertos = 25;
+        int numeroErros = 30;
+        String conteudoAcertos= "Acertos ("+Integer.toString(numeroAcertos)+")";
+        String conteudoErros= "Erros ("+Integer.toString(numeroErros)+")";
+
+        txtAcertos.setText(conteudoAcertos);
+        txtErros.setText(conteudoErros);
+
+        graficoPizzaEstatisticas.addPieSlice(new PieModel("Acertos", numeroAcertos,
+                Color.parseColor("#835AD2")));
+
+        graficoPizzaEstatisticas.addPieSlice(new PieModel("Erros", numeroErros,
+                Color.parseColor("#CB6CE6")));
+
+        graficoPizzaEstatisticas.startAnimation();
     }
 }
