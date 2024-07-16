9c09205e40d1e879f3c859da8a37eecb104e3c9c
noemi170 <2022335728@ifam.edu.br>
2024-07-16 16:02:19 -0300

    Alteracoes da main

--- app/src/main/java/com/example/helperinolympics/CadastroAlunoProfessorActivity.java
+++ app/src/main/java/com/example/helperinolympics/CadastroAlunoProfessorActivity.java
@@ -1,6 +1,7 @@
 package com.example.helperinolympics;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +9,32 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class CadastroAlunoProfessor extends AppCompatActivity {
+public class CadastroAlunoProfessorActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_cadastro_aluno_professor);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        findViewById(R.id.btnSouProfessor).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showNotification();
+            }
+        });
+    }
+
+    private void showNotification() {
+        FlashcardModelo notificationDialogFragment = new FlashcardModelo();
+        notificationDialogFragment.show(getSupportFragmentManager(), "notificationDialog");
     }
 }
--- app/src/main/java/com/example/helperinolympics/RecadoProfWebActivity.java
+++ app/src/main/java/com/example/helperinolympics/RecadoProfWebActivity.java
@@ -1,2 +1,35 @@
-package com.example.helperinolympics;public class RecadoProfWebActivity {
+package com.example.helperinolympics;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+public class RecadoProfWebActivity extends DialogFragment {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.activity_recado_prof_web, container, false);
+
+
+        // Configurar o botão de fechar
+        view.findViewById(R.id.btnFecharRecadoProf).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dismiss();
+            }
+        });
+
+        return view;
+    }
+
+    public void onStart() {
+        super.onStart();
+        if (getDialog() != null) {
+            getDialog().getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+            getDialog().getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+        }
+    }
 }
