cc0f87a964984124573d81ceb369cd2a33c26bfe
noemi170 <2022335728@ifam.edu.br>
2024-07-15 21:37:19 -0300

    algumas telas e afins

--- app/src/main/java/com/example/helperinolympics/TelaBemVindo.java
+++ app/src/main/java/com/example/helperinolympics/TelaBemVindo.java
@@ -1,6 +1,8 @@
 package com.example.helperinolympics;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,12 +15,16 @@ public class TelaBemVindo extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_tela_bem_vindo);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        // Recuperar o nome do usuário passado do Intent
+        Intent intent = getIntent();
+        if (intent != null) {
+            String username = intent.getStringExtra("name");
+
+            TextView textViewUsername = findViewById(R.id.ediTextNomeCompleto);
+            textViewUsername.setText("Bem-vindo, " +username);
+        }
+
     }
 }
