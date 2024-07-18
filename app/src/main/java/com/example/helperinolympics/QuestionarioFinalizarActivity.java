131409847ae98d67a7bbc060f95a19c9ebdb3c47
Isabela Cunha Sevalho <2022326890@ifam.edu.br>
2024-07-18 18:25:21 -0300

    Adding images

--- app/src/main/java/com/example/helperinolympics/QuestionarioFinalizarActivity.java
+++ app/src/main/java/com/example/helperinolympics/QuestionarioFinalizarActivity.java
@@ -1,3 +1,12 @@
 package com.example.helperinolympics;
 
-public class 
+import android.app.Activity;
+import android.os.Bundle;
+
+public class QuestionarioFinalizarActivity extends Activity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_questionario_finalizar);
+    }
+}
