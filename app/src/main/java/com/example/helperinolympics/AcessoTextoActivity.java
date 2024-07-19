ae381fa491aacb07610bd1f212d7f0469273a7be
Isabela Cunha Sevalho <2022326890@ifam.edu.br>
2024-07-19 18:28:43 -0300

    Changing screens

--- app/src/main/java/com/example/helperinolympics/AcessoTextoActivity.java
+++ app/src/main/java/com/example/helperinolympics/AcessoTextoActivity.java
@@ -8,13 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class TelaCadastroActivity extends AppCompatActivity {
+public class AcessoTextoActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_cadastro);
+        setContentView(R.layout.activity_acesso_texto);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
--- app/src/main/java/com/example/helperinolympics/menu/ConfiguracoesActivity.java
+++ app/src/main/java/com/example/helperinolympics/menu/ConfiguracoesActivity.java
@@ -1,4 +1,19 @@
 package com.example.helperinolympics.menu;
 
-public class ConfiguracoesActivity {
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.helperinolympics.R;
+
+public class ConfiguracoesActivity extends Activity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_configuracoes);
+    }
 }
