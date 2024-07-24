5e3dfa44bbc6b4eeb187eeec135efc65f1a3dc39
Isabela Cunha Sevalho <2022326890@ifam.edu.br>
2024-07-24 15:07:05 -0300

    Trying to implement the function of clicking on a list item and displaying a new screen

--- app/src/main/java/com/example/helperinolympics/telas_iniciais/InicialAlunoMenuDeslizanteActivity.java
+++ app/src/main/java/com/example/helperinolympics/telas_iniciais/InicialAlunoMenuDeslizanteActivity.java
@@ -1,17 +1,22 @@
 package com.example.helperinolympics.telas_iniciais;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.helperinolympics.R;
 import com.example.helperinolympics.RankingActivity;
+import com.example.helperinolympics.adapter.AdapterOlimpiadas;
 import com.example.helperinolympics.databinding.ActivityMenuDeslizanteAlunoBinding;
 import com.example.helperinolympics.menu.ChatsAlunoActivity;
 import com.example.helperinolympics.menu.ConfiguracoesActivity;
@@ -19,14 +24,22 @@ import com.example.helperinolympics.menu.FavoritosAlunoActivity;
 import com.example.helperinolympics.menu.ManualActivity;
 import com.example.helperinolympics.menu.PerfilAlunoActivity;
 import com.example.helperinolympics.menu.SairActivity;
+import com.example.helperinolympics.model.DadosOlimpiada;
 import com.google.android.material.navigation.NavigationView;
 
-public class InicialAlunoMenuDeslizanteActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class InicialAlunoMenuDeslizanteActivity extends AppCompatActivity{
 
     private DrawerLayout drawerLayout;
     private ImageButton btnAbreFechaMenu, acessarRanking, acessarCalendario;
     private NavigationView navView;
     private ActivityMenuDeslizanteAlunoBinding binding;
+
+    RecyclerView rvOlimpiadas;
+    List<DadosOlimpiada> olimpiadas = new ArrayList<>();
+    AdapterOlimpiadas adapter;
 
 
     @Override
@@ -94,13 +107,52 @@ public class InicialAlunoMenuDeslizanteActivity extends AppCompatActivity {
          }
      });
 
-     //Acesso a olimpiada
-        findViewById(R.id.cardOlimpiada2).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(InicialAlunoMenuDeslizanteActivity.this, InicioOlimpiadaActivity.class);
-                startActivity(intent);
-            }
-        });
+        configurarRecyclerOlimpiadas();
     }
+
+    public void configurarRecyclerOlimpiadas(){
+        LinearLayoutManager layoutManager= new LinearLayoutManager(this);
+        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        adapter= new AdapterOlimpiadas(olimpiadas);
+        rvOlimpiadas=findViewById(R.id.recyclerViewTelaInicialOlimpiadas);
+        rvOlimpiadas.setLayoutManager(layoutManager);
+        rvOlimpiadas.setHasFixedSize(true);
+        rvOlimpiadas.setAdapter(adapter);
+
+        //DADOS PARA O BANCO
+        DadosOlimpiada dado1 = new DadosOlimpiada(R.drawable.imgtelescopio, "Olimpíada Brasileira de Astronomia",
+                "OBA", "Rosa");
+        olimpiadas.add(dado1);
+
+        DadosOlimpiada dado2 = new DadosOlimpiada(R.drawable.imgmacacaindo, "Olimpíada Brasileira de Física",
+                "OBF", "Azul");
+        olimpiadas.add(dado2);
+
+        DadosOlimpiada dado3 = new DadosOlimpiada(R.drawable.imgcomputador, "Olimpíada Brasileira de Informática",
+                "OBI", "Laranja");
+        olimpiadas.add(dado3);
+
+        //CONFIGURAR PARA AUMENTAR SE FOR OBMEP
+        DadosOlimpiada dado4 = new DadosOlimpiada(R.drawable.imgoperacoesbasicas, "Olimpíada Brasileira de Matemática das Escolas Públicas",
+                "OBMEP", "Ciano");
+        olimpiadas.add(dado4);
+
+        DadosOlimpiada dado5 = new DadosOlimpiada(R.drawable.imgpapiro, "Olimpíada Nacional da História Brasileira",
+                "ONHB", "Rosa");
+        olimpiadas.add(dado5);
+
+        DadosOlimpiada dado6 = new DadosOlimpiada(R.drawable.imgtubodeensaio, "Olimpíada Brasileira de Química",
+                "OBQ", "Azul");
+        olimpiadas.add(dado6);
+
+        DadosOlimpiada dado7 = new DadosOlimpiada(R.drawable.imgdna, "Olimpíada Brasileira de Biologia",
+                "OBB", "Laranja");
+        olimpiadas.add(dado7);
+
+        DadosOlimpiada dado8 = new DadosOlimpiada(R.drawable.imgatomo, "Olimpíada Nacional de Ciências",
+                "ONC", "Ciano");
+        olimpiadas.add(dado8);
+    }
+
+
 }
--- app/src/main/java/com/example/helperinolympics/telas_iniciais/InicioOlimpiadaActivity.java
+++ app/src/main/java/com/example/helperinolympics/telas_iniciais/InicioOlimpiadaActivity.java
@@ -17,8 +17,7 @@ import java.util.List;
 
 public class InicioOlimpiadaActivity extends Activity {
 
-    RecyclerView rvOlimpiadas;
-    AdapterOlimpiadas adapter;
+
 
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
@@ -32,54 +31,6 @@ public class InicioOlimpiadaActivity extends Activity {
             }
         });
 
-        configurarRecyclerOlimpiadas();
     }
 
-    public void configurarRecyclerOlimpiadas(){
-        rvOlimpiadas=findViewById(R.id.recyclerViewTelaInicialOlimpiadas);
-        LinearLayoutManager layoutManager= new LinearLayoutManager(this);
-        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
-        rvOlimpiadas.setLayoutManager(layoutManager);
-        rvOlimpiadas.setHasFixedSize(true);
-
-        List<DadosOlimpiada> olimpiadas = new ArrayList<>();
-        adapter= new AdapterOlimpiadas(olimpiadas);
-        rvOlimpiadas.setAdapter(adapter);
-
-        //DADOS PARA O BANCO
-        DadosOlimpiada dado1 = new DadosOlimpiada(R.drawable.imgtelescopio, "Olimpíada Brasileira de Astronomia",
-        "OBA", "Rosa");
-        olimpiadas.add(dado1);
-
-        DadosOlimpiada dado2 = new DadosOlimpiada(R.drawable.imgmacacaindo, "Olimpíada Brasileira de Física",
-                "OBF", "Azul");
-        olimpiadas.add(dado2);
-
-        DadosOlimpiada dado3 = new DadosOlimpiada(R.drawable.imgcomputador, "Olimpíada Brasileira de Informática",
-                "OBI", "Laranja");
-        olimpiadas.add(dado3);
-
-        //CONFIGURAR PARA AUMENTAR SE FOR OBMEP
-        DadosOlimpiada dado4 = new DadosOlimpiada(R.drawable.imgoperacoesbasicas, "Olimpíada Brasileira de Matemática das Escolas Públicas",
-                "OBMEP", "Ciano");
-        olimpiadas.add(dado4);
-
-        DadosOlimpiada dado5 = new DadosOlimpiada(R.drawable.imgpapiro, "Olimpíada Nacional da História Brasileira",
-                "ONHB", "Rosa");
-        olimpiadas.add(dado5);
-
-        DadosOlimpiada dado6 = new DadosOlimpiada(R.drawable.imgtubodeensaio, "Olimpíada Brasileira de Química",
-                "OBQ", "Azul");
-        olimpiadas.add(dado6);
-
-        DadosOlimpiada dado7 = new DadosOlimpiada(R.drawable.imgdna, "Olimpíada Brasileira de Biologia",
-                "OBB", "Laranja");
-        olimpiadas.add(dado7);
-
-        DadosOlimpiada dado8 = new DadosOlimpiada(R.drawable.imgatomo, "Olimpíada Nacional de Ciências",
-                "ONC", "Ciano");
-        olimpiadas.add(dado8);
-
-
-    }
 }
