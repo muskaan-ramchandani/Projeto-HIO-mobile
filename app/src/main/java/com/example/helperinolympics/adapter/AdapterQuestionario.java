5f11ca1fda683aa9ad1c12e996ebbe9401e7d6b4
Muskaan <2022326961@ifam.edu.br>
2024-07-25 13:09:26 -0400

    Implementing the recycler review on the questionnaire screen and creating model class and adpater questionnaire

--- app/src/main/java/com/example/helperinolympics/adapter/AdapterQuestionario.java
+++ app/src/main/java/com/example/helperinolympics/adapter/AdapterQuestionario.java
@@ -1,4 +1,51 @@
 package com.example.helperinolympics.adapter;
 
-public class AdapterQuestionario {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.helperinolympics.R;
+import com.example.helperinolympics.model.DadosQuestionario;
+
+import java.util.List;
+
+public class AdapterQuestionario extends RecyclerView.Adapter<AdapterQuestionario.QuestionarioViewHolder> {
+    private List<DadosQuestionario> listaQuestionario;
+
+    public AdapterQuestionario(List<DadosQuestionario> listaQuestionario) {
+        this.listaQuestionario = listaQuestionario;
+    }
+
+    @NonNull
+    @Override
+    public QuestionarioViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View viewItemListaQuestionario = LayoutInflater.from(parent.getContext()).inflate(R.layout.modelo_lista_materiais, parent, false);
+        return new QuestionarioViewHolder(viewItemListaQuestionario);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull QuestionarioViewHolder holder, int position) {
+        DadosQuestionario questionario = listaQuestionario.get(position);
+        holder.conteudo.setText(questionario.getTemaPertencente());
+        holder.userProf.setText(questionario.getProfessorCadastrou());
+    }
+
+    @Override
+    public int getItemCount() {
+        return listaQuestionario.size();
+    }
+
+    public static class QuestionarioViewHolder extends RecyclerView.ViewHolder {
+        TextView conteudo, userProf;
+
+        public QuestionarioViewHolder(@NonNull View itemView) {
+            super(itemView);
+            conteudo = itemView.findViewById(R.id.txtConteudo);
+            userProf = itemView.findViewById(R.id.txtUserProf);
+        }
+    }
 }
---
+++ app/src/main/java/com/example/helperinolympics/materiais/QuestionarioActivity.java
@@ -0,0 +1,59 @@
+package com.example.helperinolympics.materiais;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.helperinolympics.R;
+import com.example.helperinolympics.adapter.AdapterQuestionario;
+import com.example.helperinolympics.model.DadosQuestionario;
+import com.example.helperinolympics.telas_iniciais.InicioOlimpiadaActivity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class QuestionarioActivity extends AppCompatActivity {
+    RecyclerView rvQuestionario;
+    AdapterQuestionario adapter;
+    ImageButton botaoVoltarAOlimp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_questionario);
+
+        configurarRecyclerQuestionario();
+
+        botaoVoltarAOlimp = findViewById(R.id.imgButtonVoltar);
+        botaoVoltarAOlimp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                Intent intent = new Intent(QuestionarioActivity.this, InicioOlimpiadaActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
+    private void configurarRecyclerQuestionario() {
+        rvQuestionario = findViewById(R.id.recyclerviewQuestionario);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        rvQuestionario.setLayoutManager(layoutManager);
+
+        List<DadosQuestionario> listaQuestionario = new ArrayList<>();
+        listaQuestionario.add(new DadosQuestionario("Ponto material e corpo extenso", "material", "OBF", "Fundamentos da cinemática do ponto material"));
+        listaQuestionario.add(new DadosQuestionario("Referencial e trajetória", "material", "OBF", "Fundamentos da cinemática do ponto material"));
+        listaQuestionario.add(new DadosQuestionario("Distância percorrida e deslocamento", "material", "OBF", "Fundamentos da cinemática do ponto material"));
+
+        adapter = new AdapterQuestionario(listaQuestionario);
+        rvQuestionario.setAdapter(adapter);
+    }
+}
