e519862d5b579518f5dd85ccaa67d9ea4f513f44
Isabela Cunha Sevalho <2022326890@ifam.edu.br>
2024-07-26 21:53:58 -0300

    Implementing the latest recycler on the Olympics home screen

--- app/src/main/java/com/example/helperinolympics/telas_iniciais/InicioOlimpiadaActivity.java
+++ app/src/main/java/com/example/helperinolympics/telas_iniciais/InicioOlimpiadaActivity.java
@@ -12,8 +12,10 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.helperinolympics.R;
 import com.example.helperinolympics.adapter.AdapterConteudos;
 import com.example.helperinolympics.adapter.AdapterLivros;
+import com.example.helperinolympics.adapter.AdapterProvasAnteriores;
 import com.example.helperinolympics.model.DadosConteudo;
 import com.example.helperinolympics.model.DadosLivros;
+import com.example.helperinolympics.model.DadosProvasAnteriores;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -25,9 +27,11 @@ public class InicioOlimpiadaActivity extends AppCompatActivity {
 
     List<DadosConteudo> conteudos = new ArrayList<>();
     List<DadosLivros> livros = new ArrayList<>();
+    List<DadosProvasAnteriores> provas = new ArrayList<>();
     RecyclerView rvConteudos, rvLivros, rvProvasAnteriores;
-    AdapterConteudos adapterConteudos, adapterProvasAnteriores;
+    AdapterConteudos adapterConteudos;
     AdapterLivros adapterLivros;
+    AdapterProvasAnteriores adapterProvasAnteriores;
 
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
@@ -44,49 +48,7 @@ public class InicioOlimpiadaActivity extends AppCompatActivity {
 
         configurarRecyclerConteudos();
         configurarRecyclerLivros();
-    }
-
-    private void configurarRecyclerLivros() {
-        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-
-        LinearLayoutManager layoutManager= new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
-        adapterLivros= new AdapterLivros(livros);
-        rvLivros=findViewById(R.id.recyclerViewLivros);
-        rvLivros.setLayoutManager(layoutManager);
-        rvLivros.setHasFixedSize(true);
-        rvLivros.setAdapter(adapterLivros);
-
-        //DADOS FICTICIOS
-        Date dataPublicacao1 = null;
-        try {
-            // Converta a String para Date
-            dataPublicacao1 = sdf.parse("22/02/2022");
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-        DadosLivros dado1 = new DadosLivros(1, R.drawable.imglivrofisica, "O Livro da Física", "Maria Souza" ,"3", dataPublicacao1);
-        livros.add(dado1);
-
-        Date dataPublicacao2 = null;
-        try {
-            // Converta a String para Date
-            dataPublicacao2 = sdf.parse("13/03/2013");
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-        DadosLivros dado2 = new DadosLivros(2, R.drawable.imglivrofisica, "O Livro 2", "Mariaana" ,"7", dataPublicacao2);
-        livros.add(dado2);
-
-        Date dataPublicacao3 = null;
-        try {
-            // Converta a String para Date
-            dataPublicacao3 = sdf.parse("01/01/1991");
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-        DadosLivros dado3 = new DadosLivros(3, R.drawable.imglivrofisica, "OUTRO", "Magali" ,"1", dataPublicacao3);
-        livros.add(dado3);
-
+        configurarRecyclerProvas();
     }
 
     public void configurarRecyclerConteudos(){
@@ -133,6 +95,76 @@ public class InicioOlimpiadaActivity extends AppCompatActivity {
         } else {
             Log.e("InicialAlunoMenuDeslizanteActivity", "Nenhuma lista de olimpiadas foi recebida.");
         }*/
+    }
+
+    private void configurarRecyclerLivros() {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+        LinearLayoutManager layoutManager= new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        adapterLivros= new AdapterLivros(livros);
+        rvLivros=findViewById(R.id.recyclerViewLivros);
+        rvLivros.setLayoutManager(layoutManager);
+        rvLivros.setHasFixedSize(true);
+        rvLivros.setAdapter(adapterLivros);
+
+        //DADOS FICTICIOS
+        Date dataPublicacao1 = null;
+        try {
+            // Converta a String para Date
+            dataPublicacao1 = sdf.parse("22/02/2022");
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        DadosLivros dado1 = new DadosLivros(1, R.drawable.imglivrofisica, "O Livro da Física", "Maria Souza" ,"3", dataPublicacao1);
+        livros.add(dado1);
+
+        Date dataPublicacao2 = null;
+        try {
+            // Converta a String para Date
+            dataPublicacao2 = sdf.parse("13/03/2013");
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        DadosLivros dado2 = new DadosLivros(2, R.drawable.imglivrofisica, "O Livro 2", "Mariaana" ,"7", dataPublicacao2);
+        livros.add(dado2);
+
+        Date dataPublicacao3 = null;
+        try {
+            // Converta a String para Date
+            dataPublicacao3 = sdf.parse("01/01/1991");
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        DadosLivros dado3 = new DadosLivros(3, R.drawable.imglivrofisica, "OUTRO", "Magali" ,"1", dataPublicacao3);
+        livros.add(dado3);
+
+    }
+
+    private void configurarRecyclerProvas() {
+        LinearLayoutManager layoutManager= new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        adapterProvasAnteriores= new AdapterProvasAnteriores(provas);
+        rvProvasAnteriores=findViewById(R.id.recyclerViewProvasAnteriores);
+        rvProvasAnteriores.setLayoutManager(layoutManager);
+        rvProvasAnteriores.setHasFixedSize(true);
+        rvProvasAnteriores.setAdapter(adapterProvasAnteriores);
+
+
+        //DADOS FICTICIOS
+        DadosProvasAnteriores dado1 = new DadosProvasAnteriores(1, 2022, 3, true, "demiLov");
+        provas.add(dado1);
+
+        DadosProvasAnteriores dado2 = new DadosProvasAnteriores(2, 2019, 2, false, "doroteia");
+        provas.add(dado2);
+
+        DadosProvasAnteriores dado3 = new DadosProvasAnteriores(3, 2006, 1, false, "luanSantana");
+        provas.add(dado3);
+
+        DadosProvasAnteriores dado4 = new DadosProvasAnteriores(4, 2020, 5, true, "picasso");
+        provas.add(dado4);
+
+        DadosProvasAnteriores dado5 = new DadosProvasAnteriores(5, 2015, 1, false, "marianaContaUm");
+        provas.add(dado5);
+
     }
 
 }
