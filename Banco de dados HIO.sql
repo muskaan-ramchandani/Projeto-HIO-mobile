9987aef86d455af554adcfbb5074c73443676815
Isabela Cunha Sevalho <141193230+IsabelaSevalho@users.noreply.github.com>
2024-09-08 16:00:31 -0400

    Mysql database file
    This version only has what is already possible to register in the database

---
+++ Banco de dados HIO.sql
@@ -0,0 +1,37 @@
+CREATE DATABASE hio;
+DROP DATABASE IF exists HIO;
+USE hio;
+
+DROP TABLE IF EXISTS Olimpiada;
+
+CREATE TABLE Olimpiada(
+    nome VARCHAR(300) NOT NULL,
+    sigla VARCHAR(10) NOT NULL,
+    icone VARCHAR(700) NOT NULL,
+    cor VARCHAR(10) NOT NULL,
+    PRIMARY KEY(sigla)
+);
+
+SELECT * FROM Olimpiada;
+INSERT INTO Olimpiada VALUES
+    ('Olimpíada Brasileira de Astronomia', 'OBA', 'imgtelescopio', 'Rosa'),
+    ('Olimpíada Brasileira de Física', 'OBF', 'imgmacacaindo', 'Azul'),
+    ('Olimpíada Brasileira de Informática', 'OBI', 'imgcomputador', 'Laranja'),
+    ('Olimpíada Brasileira de Matemática das Escolas Públicas', 'OBMEP', 'imgoperacoesbasicas', 'Ciano'),
+    ('Olimpíada Nacional da História Brasileira', 'ONHB', 'imgpapiro', 'Rosa'),
+    ('Olimpíada Brasileira de Química', 'OBQ', 'imgtubodeensaio', 'Azul'),
+    ('Olimpíada Brasileira de Biologia', 'OBB', 'imgdna', 'Laranja'),
+    ('Olimpíada Nacional de Ciências', 'ONC', 'imgatomo', 'Ciano');
+
+
+DROP TABLE IF EXISTS Aluno;
+SELECT * FROM Aluno;
+CREATE TABLE Aluno(
+	nomeCompleto VARCHAR(200) NOT NULL,
+    nomeUsuario VARCHAR(10) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    senha VARCHAR(20) NOT NULL,
+    fotoPerfil LONGBLOB,
+    PRIMARY KEY(email)
+);
+
